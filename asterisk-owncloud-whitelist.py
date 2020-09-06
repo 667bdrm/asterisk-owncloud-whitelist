b922eed6763e2f2bc3e7364b22a3663ed4597e0e
667bdrm <root@localhost>
2020-09-06 11:50:19 +0300

    Writing only found contacts in regular log, writing all calls to debug log. Setting caller id name variable for next extensions use.

--- asterisk-owncloud-whitelist.py
+++ asterisk-owncloud-whitelist.py
@@ -41,6 +41,15 @@ def recursive_glob(treeroot, pattern):
         results.extend(os.path.join(base, f) for f in goodfiles)
     return results
 
+def log_agi_env(fp):
+    fp.write("\nIncoming call\nData source: %s\n" % host)
+    fp.write(datetime.datetime.now().strftime("%Y-%m-%d %H:%M") + "\n")
+
+    for k in env.keys():
+        if k.find('agi_arg_') == -1:
+            fp.write(k + ' = ' + env[k] + "\n")
+
+
 while 1:
    line = sys.stdin.readline().strip()
 
@@ -64,7 +73,7 @@ for key in env.keys():
    sys.stderr.flush()
 
 f = open(os.environ.get('AST_LOG_DIR', '') + '/agi_owncloud_whitelist.log', 'a+')
-
+df = open(os.environ.get('AST_LOG_DIR', '') + '/agi_owncloud_whitelist_debug.log', 'a+')
 
 
 host = ""
@@ -82,13 +91,7 @@ if len(sys.argv) >= 5:
     blacklist_sounds_path = sys.argv[5]
     blacklist_category = sys.argv[6]
 
-    
-f.write("\nIncoming call\nData source: %s\n" % host)
-f.write(datetime.datetime.now().strftime("%Y-%m-%d %H:%M") + "\n")
-
-for k in env.keys():
-    if k.find('agi_arg_') == -1:
-        f.write(k + ' = ' + env[k] + "\n")
+log_agi_env(df)
 
 black_found = False
 
@@ -115,7 +118,7 @@ if len(blacklist_sounds) > 1:
     blacklist_sound_index = random.randint(0, random_max)
     blacklist_sound = blacklist_sounds[blacklist_sound_index].replace(sounds_path + '/', '').replace('.gsm', '')
     
-f.write("Requesting contacts\n")
+df.write("Requesting contacts\n")
 
 
 request_url = 'https://' + host + path
@@ -124,7 +127,7 @@ try:
 
     response = requests.request('REPORT', request_url, data=xml_data, headers=headers, auth=(user, password))
 except Exception as ex:
-    f.write("Error requesting contacts")
+    df.write("Error requesting contacts")
     f.write(str(ex))
 
 
@@ -132,10 +135,10 @@ except Exception as ex:
 try:
     data = lxml.etree.fromstring(response.text.encode('utf-8', 'ignore'))
 except Exception as ex:
-    f.write("Error loading contacts\n")
-    f.write(str(ex))
+    df.write("Error loading contacts\n")
+    df.write(str(ex))
     
-f.write("Loading contacts\n")
+df.write("Loading contacts\n")
 
 contacts = []
 
@@ -143,11 +146,12 @@ try:
 
     contacts =  data.xpath('//*/card:address-data', namespaces = {"d":"DAV:", "card":"urn:ietf:params:xml:ns:carddav"})
 except:
-    f.write('Cannot load contacts')
+    df.write('Cannot load contacts')
 
 contact_found = False
 
 black_found = False
+caller_name = ''
 
 for contact_elem in contacts:
     
@@ -156,8 +160,8 @@ for contact_elem in contacts:
     try:
         name = vcard.contents.get('fn',[])[0].value.encode('utf-8')
     except Exception as ex:
-        f.write("Failed to get contact name\n")
-        f.write(str(ex))
+        df.write("Failed to get contact name\n")
+        df.write(str(ex))
     
     for tel in vcard.contents.get('tel', []):
         raw_phone = tel.value
@@ -169,43 +173,47 @@ for contact_elem in contacts:
         # if metch tel:
         if pattern.search(callerid):
             contact_found = True
+            log_agi_env(f)
             f.write("Found contact for %s\n" % phone)
 
             try:
                 f.write("Contact: %s\n" % name)
-           
+                caller_name = name
               
                 cats_data =  vcard.contents.get('categories', [])
     
                 cats = []
-    
+                
                 if len(cats_data) > 0:
                     cats = cats_data[0].value
-        
+                
                 for cat in cats:
-        
-                    if cat == blacklist_category:
-                        f.write("Blacklist found: %s %s\n" % (name, phone))
-                        f.write("Selected sound = " + blacklist_sound + "\n")
-                        black_found = True
-        
+                    if cat != None and blacklist_category != None:
+                        if cat.encode('utf-8') == blacklist_category:
+                            f.write("Blacklist found: %s %s\n" % (name, phone))
+                            f.write("Selected sound = " + blacklist_sound + "\n")
+                            black_found = True
+
             except Exception as ex:
                 f.write("Failed to get contact categories\n")
                 f.write(str(ex))
             
 if len(contacts) == 0:
-    print "NOOP\n"
+    print("NOOP\n")
+elif contact_found == True and black_found == False and caller_name != '':
+    print("SET VARIABLE CALLER_NAME \"%s\"\n" % caller_name)
 elif (contact_found == True and black_found == True) or (env.get('agi_calleridname') == 'collector'):
-    print "ANSWER\n"
-    print "EXEC WAIT \"4\"\n"
-    print "EXEC PLAYBACK \"" + blacklist_sound + "\"\n"
-    print "EXEC WAIT \"10\"\n"
-    print "HANGUP\n"
+    print("ANSWER\n")
+    print("EXEC WAIT \"4\"\n")
+    print("EXEC PLAYBACK \"" + blacklist_sound + "\"\n")
+    print("EXEC WAIT \"10\"\n")
+    print("HANGUP\n")
 elif contact_found == False:
-    print "ANSWER\n"
-    print "EXEC PLAYBACK \"followme/sorry\"\n"
-    print "HANGUP\n"
-    f.write("Contact not found: %s\n" % env.get('agi_callerid'))
+    print("ANSWER\n")
+    print("SET VARIABLE NO_CONTACT \"1\"\n")
+    print "EXEC PLAYBACK \"unavailable-gsm\"\n"
+    print("HANGUP\n")
+    df.write("Contact not found: %s\n" % env.get('agi_callerid'))
 
 f.close()
-
+df.close()
